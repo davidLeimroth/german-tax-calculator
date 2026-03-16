1c0f3e022d07f171f7f13038701191feb74a2075
David Leimroth <david.leimroth@semrush.com>
2026-03-16 23:55:00 +0100

    change path to match repo

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import tailwindcss from '@tailwindcss/vite';
 // https://astro.build/config
 export default defineConfig({
   output: 'static',
-  base: '/steuerrechner',
+  base: '/german-tax-calculator',
   integrations: [react()],
   vite: {
     plugins: [tailwindcss()],
